6845cf9a219875771ee1290dfeb653fbe204ccd9
AbdulRafay-10 <potatogameryt996@gmail.com>
2025-10-27 18:04:27 +0500

    fix

--- src/components/aboutus/LeadershipTeam.jsx
+++ src/components/aboutus/LeadershipTeam.jsx
@@ -9,10 +9,9 @@ import taha from "../../assets/Team-Members/taha.jpeg"
 const team = [
   { name: 'Awais Khan', title: 'Founder & CEO', img: p1, imgPosition: 'center 30%' }, 
   { name: 'Anees Khan', title: 'CEO', img: p2, imgPosition: 'center 0%' },
-  { name: 'Taha Khan', title: 'Project Manager', img: taha , imgPosition: 'center 0%' },
+  { name: 'Taha Khan', title: 'Project Manager', img: taha  },
   { name: 'Abdul Rafay', title: 'Full Stack Developer', img: p3, imgPosition: 'center 10%' },
-  { name: 'Sulaiman Khan', title: 'UI/UX Designer', img: sulaiman , imgPosition: 'center 0%' },
-  
+  { name: 'Sulaiman Khan', title: 'UI/UX Designer', img: sulaiman  },
 ]
 
 function LeadershipTeam() {
